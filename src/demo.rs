db05715ce45c76d00743e31da72044182e7576ba
Tarun Verghis <tarun.verghis@gmail.com>
2022-10-05 08:35:50 -0700

    Check for magic header before reading the whole file into memory

--- src/demo.rs
+++ src/demo.rs
@@ -17,12 +17,24 @@ impl Demo {
     /// Construct a new `Demo` from anything that implements [std::io::Read].
     /// This can fail, for example, if the bytes do not start with the magic header.
     pub fn try_from_read<R: Read>(r: &mut R) -> Result<Self, MithrilErrorKind> {
-        // TODO: Check if the demo is valid first before reading the whole thing into memory.
+        // Read the first 8 bytes and ensure they match the magic header.
+        let mut header_buf = [0; 8];
+        r.read_exact(&mut header_buf)?;
 
+        if header_buf != Self::SOURCE2_MAGIC_BYTES {
+            return Err(MithrilErrorKind::InvalidDemo);
+        }
+
+        // Skip the next 8 bytes.
+        r.read_exact(&mut [0; 8])?;
+
+        // The remaining bytes constitute the actual demo data.
         let mut buf = Vec::new();
         r.read_to_end(&mut buf)?;
 
-        Self::try_from(buf)
+        let demo = Self { bytes: buf };
+
+        Ok(demo)
     }
 
     /// A read-only view into the inner data.
@@ -31,71 +43,46 @@ impl Demo {
     }
 }
 
-/// Try to create a `Demo` from arbitrary bytes.
-/// This can fail, for example, if the bytes do not start with the magic header.
-impl TryFrom<Vec<u8>> for Demo {
-    type Error = MithrilErrorKind;
-
-    fn try_from(bytes: Vec<u8>) -> Result<Self, Self::Error> {
-        if !is_valid_demo(&bytes) {
-            return Err(MithrilErrorKind::InvalidDemo);
-        }
-
-        let replay = Self { bytes };
-
-        Ok(replay)
-    }
-}
-
-// Checks that the demo starts with the expected magic header bytes.
-fn is_valid_demo(demo_bytes: &[u8]) -> bool {
-    demo_bytes.starts_with(&Demo::SOURCE2_MAGIC_BYTES)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    mod validate {
-        use super::*;
-
-        #[test]
-        fn empty_demo_is_invalid() {
-            assert!(!is_valid_demo(&[]));
-        }
-
-        #[test]
-        fn insufficient_magic_bytes_is_invalid_demo() {
-            assert!(!is_valid_demo(&[b'P', b'B', b'D']));
-        }
-
-        #[test]
-        fn incorrect_magic_bytes_is_invalid_demo() {
-            assert!(!is_valid_demo(&[0xFF; 8]));
-        }
-    }
 
     mod try_from_bytes {
         use super::*;
 
         #[test]
         #[should_panic]
+        fn fails_for_empty_demo() {
+            let bytes = vec![];
+            let _ = Demo::try_from_read(&mut bytes.as_slice()).unwrap();
+        }
+
+        #[test]
+        #[should_panic]
         fn fails_for_invalid_demo() {
             let bytes = vec![0xFF, 0xFF];
-            let _ = Demo::try_from(bytes).unwrap();
+            let _ = Demo::try_from_read(&mut bytes.as_slice()).unwrap();
+        }
+
+        #[test]
+        #[should_panic]
+        fn fails_for_demo_with_insufficient_bytes() {
+            let mut bytes = vec![];
+            bytes.extend(Demo::SOURCE2_MAGIC_BYTES);
+            bytes.extend([0x00, 0x00]); // Not enough bytes in the unused section, reading should fail.
+
+            let _ = Demo::try_from_read(&mut bytes.as_slice()).unwrap();
         }
 
         #[test]
         fn works_for_valid_demo() {
+            let unused_bytes = [0xDE, 0xAD, 0xC0, 0xDE, 0xDE, 0xAD, 0xC0, 0xDE];
             let data = [0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF];
-            let bytes = [Demo::SOURCE2_MAGIC_BYTES, data].concat();
+            let bytes = [Demo::SOURCE2_MAGIC_BYTES, unused_bytes, data].concat();
 
-            let demo = Demo::try_from(bytes).unwrap();
+            let demo = Demo::try_from_read(&mut bytes.as_slice()).unwrap();
 
-            assert_eq!(
-                demo.bytes().len(),
-                Demo::SOURCE2_MAGIC_BYTES.len() + data.len()
-            )
+            assert_eq!(demo.bytes(), data)
         }
     }
 }
